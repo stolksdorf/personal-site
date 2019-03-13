66e7b5f36301069e41c17a37809eeb74e456c54e
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2019-03-13 11:28:06 -0400

    test

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const config = require('./config/config.init.js');
-
 const server = require('./server/server.js');
 
 const PORT = config.get('port');
 server.listen(PORT, ()=>{
 	console.log('_____________________________');
-	console.log(`PROJECT server running on port:${PORT} 🚀 `);
+	console.log(`Personal Site running on port:${PORT} 🚀 `);
 });
